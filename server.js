fa208a1e131eb469b55f6461e5dbad88912e4a47
Vinayk93 <vinaykumar7737245904@gmail.com>
2016-04-04 16:57:52 +0530

    sad

--- server.js
+++ server.js
@@ -22,46 +22,24 @@ app.get('/api/data',function(req,res){
   fs.readFile('saved.txt',"",function(err,a){
     if(err) throw err;
     if(a)console.log(a);
-    res.send(JSON.parse(a));
+    res.send(""+a);
   })
 });
 
 app.post('/api/send',function (req,res) {
   var duration=3000;
     var temp=23;
-  //var a=req.body;
-   // console.log(a);
-  /* 
+ 
+   var a=req.body;
+   console.log(a);
+  
     var k={
             "msg_id": "deb1317c-e67b-417b-a407-08d38175fef0",
             "_text": "send to set ac to thirty minutes",
             "outcomes":[a]
           }; 
-     
-          //commend it now
-          */
-          var k= {
-  "msg_id": "4a65dee8-c518-47aa-a211-d6d7eb169c30",
-  "_text": "set at freezing point",
-  "outcomes": [
-    {
-      "_text": "set at freezing point",
-      "confidence": 0.946,
-      "intent": "ac",
-      "entities": {
-        "default": [
-          {
-            "value": "0 C"
-          }
-        ]
-      }
-    }
-  ]
-};
+         
 
-      //comment take off
-          console.log(k.outcomes[0].entities.how_are_you_);
-  //var k=req.body;
 	var result=new Array();
 var count=0;
 console.log(k);
@@ -100,7 +78,10 @@ console.log(k);
   res.send(JSON.parse('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"off"}'));
   var a=JSON.parse('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"off"}');
 	}
+
 	else if (k.outcomes[0].intent=='ac'){
+    var temp=999;
+    var duration=999;
     console.log("take value of ac");
 		if(k.outcomes[0].entities.duration){
 		for(var a=0;a<k.outcomes[0].entities.duration.length;a++){
@@ -113,12 +94,15 @@ console.log(k);
 		if(k.outcomes[0].entities.temperature){
     for(var a=0;a<k.outcomes[0].entities.temperature.length;a++){
       if(k.outcomes[0].entities.temperature[a].value){
-        temp=k.outcomes[0].entities.temperature[a].value;
+        temp=k.outcomes[0].entities.temperature[a].value; 
         result[0]="Air Conditioner is set to "+temp+" Celsius";
+      
       }
     }
   }
+
   if(k.outcomes[0].entities.default){
+    console.log("on default");
     for(var a=0;a<k.outcomes[0].entities.default.length;a++){
       if(k.outcomes[0].entities.default[a].value){
         temp=k.outcomes[0].entities.default[a].value;
@@ -126,27 +110,22 @@ console.log(k);
       }
     }
   }
-
-
-  //save it on cookie
-  if(temp==null){
-    var a=JSON.parse('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"on"}');
-    res.send(JSON.parse('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"on"}'));
-    }
-    else{ 
-    console.log(result+" -"+duration+"-"+temp);
-  var a='{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"off"}';
-   // var a=JSON.parse('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"on"}');
-   
-    res.send('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"on"}');
- 
-    //var s=new Buffer(ss);
-    //console.log(ss);
-    //var p=ss;
-    }
+  console.log(duration+temp);
+  console.log(temp.toString()=== 'null');
+  if(temp.toString()!== 'null'){
+    console.log("define ac");
+  var a=(JSON.parse('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"on"}'));
+  res.send(JSON.parse('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"on"}'));
+  }else{
+    console.log("stop ac");
+    result[0]="Air Conditioner switch off";
+var a=(JSON.parse('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"off"}'));
+  res.send(JSON.parse('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"off"}'));
   }
-//res.send(a);
+    }
   
+  
+//res.send(a);
   var k1=JSON.stringify(a);
   console.log(a);
   console.log(k1);
