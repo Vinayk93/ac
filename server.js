8ecf7019bb7c03f6f4d24a42b904f9003384046f
Vinayk93 <vinaykumar7737245904@gmail.com>
2016-04-04 16:10:06 +0530

    hmm

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var cookieParser    = require('cookie-parser');
 var fs    = require('fs');
 app.use(bodyParser.json()); 
 app.use(morgan('dev')); 
-var port=8080;            
+var port=3000;            
 
 
 app.use(morgan('dev'));
@@ -22,10 +22,9 @@ app.get('/api/data',function(req,res){
   fs.readFile('saved.txt',"",function(err,a){
     if(err) throw err;
     if(a)console.log(a);
-    res.send(""+a);
+    res.send(JSON.parse(a));
   })
 });
-
 
 app.post('/api/send',function (req,res) {
   var duration=3000;
@@ -42,17 +41,17 @@ app.post('/api/send',function (req,res) {
           //commend it now
           */
           var k= {
-  "msg_id": "d567989c-1da5-46b2-95c5-43b83d61cd86",
-  "_text": "hi",
+  "msg_id": "4a65dee8-c518-47aa-a211-d6d7eb169c30",
+  "_text": "set at freezing point",
   "outcomes": [
     {
-      "_text": "hi",
-      "confidence": 0.546,
-      "intent": "greetings",
+      "_text": "set at freezing point",
+      "confidence": 0.946,
+      "intent": "ac",
       "entities": {
-        "hello": [
+        "default": [
           {
-            "value": "hey"
+            "value": "0 C"
           }
         ]
       }
@@ -89,9 +88,9 @@ console.log(k);
 		for(var b=0;b<k.outcomes[0].entities.I_am_fine.length;b++){
 			if(k.outcomes[0].entities.I_am_fine[b].value){
 				//console.log(k.outcomes[0].entities.I_am_fine[b].value);
-				++count;
         result[count]=k.outcomes[0].entities.I_am_fine[b].value;
-			}
+			++count;
+      }
 		}
   } 
 			var temp=999;
@@ -107,29 +106,39 @@ console.log(k);
 		for(var a=0;a<k.outcomes[0].entities.duration.length;a++){
 			if(k.outcomes[0].entities.duration[a].value){
 				duration=k.outcomes[0].entities.duration[a].normalized.value;
-				result[0]="OK";
+				result[0]="Air Conditioner is set for "+duration+" Second";
 			}
 		}
 	}
 		if(k.outcomes[0].entities.temperature){
-		for(var a=0;a<k.outcomes[0].entities.temperature.length;a++){
-			if(k.outcomes[0].entities.temperature[a].value){
-				temp=k.outcomes[0].entities.temperature[a].value;
-				result[0]="Ac is set to "+temp+" second";
-			}
-		}
-	}
+    for(var a=0;a<k.outcomes[0].entities.temperature.length;a++){
+      if(k.outcomes[0].entities.temperature[a].value){
+        temp=k.outcomes[0].entities.temperature[a].value;
+        result[0]="Air Conditioner is set to "+temp+" Celsius";
+      }
+    }
+  }
+  if(k.outcomes[0].entities.default){
+    for(var a=0;a<k.outcomes[0].entities.default.length;a++){
+      if(k.outcomes[0].entities.default[a].value){
+        temp=k.outcomes[0].entities.default[a].value;
+        result[0]="Air Conditioner is set to "+temp+" Celsius";
+      }
+    }
+  }
+
 
   //save it on cookie
   if(temp==null){
-    var a='{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"on"}';
+    var a=JSON.parse('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"on"}');
     res.send(JSON.parse('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"on"}'));
     }
-    else{
-    //console.log('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status","on"}');
-
-    var a='{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"on"}';
-    res.send(JSON.parse('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"on"}'));
+    else{ 
+    console.log(result+" -"+duration+"-"+temp);
+  var a='{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"off"}';
+   // var a=JSON.parse('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"on"}');
+   
+    res.send('{"status":"true","data":"'+result+'","duration":'+duration+',"temp":'+temp+',"ac_status":"on"}');
  
     //var s=new Buffer(ss);
     //console.log(ss);
@@ -157,92 +166,4 @@ console.log('Magic happens on port ' + port);
 
 /*
 
-  var ra=JSON.stringify(
-/*
-    {
-  "msg_id": "deb1317c-e67b-417b-a407-08d38175fef0",
-  "_text": "send to set ac to thirty minutes",
-  "outcomes": [
-    {
-      "_text": "send to set ac to thirty minutes",
-      "confidence": 0.695,
-      "intent": "ac",
-      "entities": {
-        "duration": [{
-            "minute": 30,
-            "value": 30,
-            "unit": "minute",
-            "normalized": {
-              "value": 1800,
-              "unit": "second"
-            }
-          }
-        ]
-      }
-    }
-  ]
-}
-
-/*
-{
-  "msg_id": "1bc39061-d3a5-4fbe-91c0-e8cf2c8b6f53",
-  "_text": "is that runs how are you",
-  "outcomes": [
-    {
-      "_text": "is that runs how are you",
-      "confidence": 0.689,
-      "intent": "greetings",
-      "entities": {
-        "how_are_you_": [
-          {
-            "value": "how are you"
-          }
-        ],
-        "I_am_fine": [
-          {
-            "value": "i am fine"
-          }
-        ]
-      }
-    }
-  ]
-}
-
-
-
-
-{
-  "msg_id": "f618841e-43c8-4513-a7fc-a48aa88fb28c",
-  "_text": "set temperature to thirty",
-  "outcomes": [
-    {
-      "_text": "set temperature to thirty",
-      "confidence": 0.694,
-      "intent": "ac",
-      "entities": {
-        "temperature": [
-          {
-            "type": "value",
-            "value": 12
-          }
-        ], "how_are_you_": [
-          {
-            "value": "how are you"
-          }
-        ],
-        "duration": [{
-            "minute": 30,
-            "value": 30,
-            "unit": "minute",
-            "normalized": {
-              "value": 18000,
-              "unit": "second"
-            }
-          }
-        ]
-      }
-    }
-  ]
-}
-);
 */
